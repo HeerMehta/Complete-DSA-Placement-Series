786382255b4804599d75d8c8e3a303bf67aabbc1
HeerMehta <62586389+HeerMehta@users.noreply.github.com>
2023-11-04 21:35:24 +0530

    Time: 7 ms (55.63%), Space: 9.7 MB (76.57%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+
+        for(int i=0; i<32; i++){
+            int sum = 0;
+            for(int j=0; j<nums.size(); j++){
+                sum += ((nums[j]>>i)&1);
+            }
+            sum %= 3;
+            res = res | (sum<<i);
+        }
+
+        return res;
+    }
+};
